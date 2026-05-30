f3ab2a310e3b4dc41953438dd33792f433c61c60
Yvonne Ross Blanco <140624973+yvonnerossy@users.noreply.github.com>
2026-05-29 22:28:07 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_URL = 'https://lmzxougaofomhyvrfeky.supabase.co';
+window.SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtenhvdWdhb2ZvbWh5dnJmZWt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMDg0NDUsImV4cCI6MjA5NTY4NDQ0NX0.kjWEh5XavzgCsdEsIvmEc912WmwHPX_bFF4tzqn2V5E';
+window.ADMIN_USER = 'admin';
+window.ADMIN_PASS = 'admin123';
